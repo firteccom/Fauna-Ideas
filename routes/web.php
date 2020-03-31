c036aae16dc8ced611e04c53077c61a21e2e5f5f
Angello Del Carpio Bravo <adelcarpio@firteccom.com>
2020-03-30 22:47:13 -0500

    Se empieza a subir cambios de pagina de ficha de productos en el frontend

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'Front\HomeController@_index'); //http://localhost/Fauna-Ideas/public
+Route::get('products','Front\ProductController@showView');
 
 
 Route::namespace('Admin')->prefix('admin')->group(function () {
